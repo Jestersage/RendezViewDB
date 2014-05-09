e1f642e4ca1444d699a9d4e15a25563adb3f5e56
Jestersage <gleung4@gmail.com>
2014-05-09 13:34:10 -0700

    updated the basic queries

--- queries.sql
+++ queries.sql
@@ -2,4 +2,10 @@
 --given userID, find the friend of user
 SELECT f.userID
 FROM friendlist f
-WHERE 
+WHERE f.ownerID = 'a00112234'
+;
+--
+--add friend
+INSERT INTO friendlist
+VALUES('a00112234', 'a00223456')
+;
